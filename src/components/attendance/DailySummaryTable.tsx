790d55ae963d61fab027f64395c9caa57a06d897
koushikghosh6088-hue <koushikghosh6088@gmail.com>
2026-07-19 15:50:48 +0530

    fix: Add missing React import in DailySummaryTable

--- src/components/attendance/DailySummaryTable.tsx
+++ src/components/attendance/DailySummaryTable.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { StatusBadge } from '@/components/ui/StatusBadge'
 import { resolveAttendanceFlag } from '@/app/actions/attendance'
